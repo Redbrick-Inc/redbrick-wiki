5296b5ee7b5f0e33ffce1d105963edde5d093306
Tim-redbrick <tim@redbrick.space>
2024-01-11 10:30:52 +0900

    fix

--- theme.config.tsx
+++ theme.config.tsx
@@ -22,7 +22,7 @@ const config: DocsThemeConfig = {
   i18n: [
     { locale: "en", name: "English" },
     { locale: "kr", name: "한국어" },
-    { locale: "my", name: "Malay" , direction: 'rtl' },
+    { locale: "my", name: "Malay", direction: "rtl" },
   ],
 };
 
