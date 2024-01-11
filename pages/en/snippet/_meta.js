4d1679f55e9f55e27763b7ae65f96be7bb0b18d5
Tim-redbrick <tim@redbrick.space>
2024-01-11 18:49:44 +0900

    add: timer example

--- pages/en/snippet/_meta.js
+++ pages/en/snippet/_meta.js
@@ -4,6 +4,7 @@ export default {
   "collect-items": "Collect items",
   spawn: "Set the Starting Point",
   respawn: "Return to original position when it falls",
+  "create-timer": "Create Timer, Countdown",
   "squid-game": "Making a stepping stone that disappears when you step on it",
   debug: "How to debug and error?",
 };
