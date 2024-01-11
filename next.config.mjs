0904c98cc42bb5d8a789d97f2c5ca8c5bb4e5bf3
Tim-redbrick <tim@redbrick.space>
2024-01-11 14:24:14 +0900

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,24 @@
-import nextra from 'nextra'
+import nextra from "nextra";
 // import { useRouter } from 'next/hooks';
 
 const withNextra = nextra({
-    theme: 'nextra-theme-docs',
-    themeConfig: './theme.config.tsx'
-    // ... your Nextra config
-})
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+
+  // ... your Nextra config
+});
 
 export default withNextra({
-    i18n: {
-        locales: ["en", "kr"],
-        defaultLocale: "en"
+  i18n: {
+    locales: ["en", "kr"],
+    defaultLocale: "en",
+  },
+  redirects: () => [
+    {
+      source: "/",
+      destination: "/en",
+      permanent: true,
     },
+  ],
+  reactStrictMode: true,
 });
--- pages/en/_meta.js
+++ pages/en/_meta.js
@@ -1,9 +1,9 @@
 export default {
-  "index": "Introduction",
-  "engine": "Redbrick Engine",
-  "script": "Text coding",
-  "block": "Block coding",
-  "faq": "FAQ",
-  "advanced": "Advanced Scripting",
-  "bugs": "Current Bugs and Issues"
-}
+  index: "Introduction",
+  engine: "Redbrick Engine",
+  script: "Text coding",
+  block: "Block coding",
+  snippet: "Snippet",
+  advanced: "Advanced Scripting",
+  bugs: "Current Bugs and Issues",
+};
