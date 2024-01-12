7c424ac501d631ed1481c3e50b6ec508684d6580
Tim-redbrick <tim@redbrick.space>
2024-01-12 10:50:48 +0900

    add: few more examples

--- pages/en/snippet/_meta.js
+++ pages/en/snippet/_meta.js
@@ -4,8 +4,11 @@ export default {
   "change-avatar-speed": "Avatar speed change ",
   spawn: "Set the Starting Point",
   respawn: "Return to original position when it falls",
+  "create-savepoints": "Create Save Point",
   "squid-game": "Making a stepping stone that disappears when you step on it",
   animations: "Creating an object that moves repeatedly",
   "create-timer": "Create Timer, Countdown",
+  "create-game-button": "Create a Game Start button",
+  "victory-gui": "Return to first screen at the end of the game",
   debug: "How to debug and error?",
 };
