b14692b704ab89c171fedd492ce1612e0e348407
Tim-redbrick <tim@redbrick.space>
2024-01-12 16:29:43 +0900

    add: korean block

---
+++ pages/kr/block/_meta.js
@@ -0,0 +1,9 @@
+export default {
+  index: "소개",
+  "general-api": "일반 API",
+  "translation-api": "이동 API",
+  "rotation-api": "회전 API",
+  "event-api": "이벤트 API",
+  "control-api": "제어 API",
+  "avatar-api": "아바타 API",
+};
