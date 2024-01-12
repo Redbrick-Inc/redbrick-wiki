c159457fc20fdf051852ed3a1fad10208a733c4f
Tim-redbrick <tim@redbrick.space>
2024-01-12 09:44:59 +0900

    add avatar speed

--- pages/en/snippet/_meta.js
+++ pages/en/snippet/_meta.js
@@ -1,10 +1,11 @@
 export default {
   "use-avatar": "How to use avatar properly",
-  animations: "Creating an object that moves repeatedly",
   "collect-items": "Collect items",
+  "change-avatar-speed": "Avatar speed change ",
   spawn: "Set the Starting Point",
   respawn: "Return to original position when it falls",
-  "create-timer": "Create Timer, Countdown",
   "squid-game": "Making a stepping stone that disappears when you step on it",
+  animations: "Creating an object that moves repeatedly",
+  "create-timer": "Create Timer, Countdown",
   debug: "How to debug and error?",
 };
