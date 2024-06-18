98e02b336de0d6e58163fa80684bd882a8120f5b
redbrick-tim <tim@redbrick.space>
2024-06-18 17:09:26 +0900

    fix: unsupported features eng/kr

--- pages/en/faq/_meta.js
+++ pages/en/faq/_meta.js
@@ -2,6 +2,6 @@ export default {
   questions: "FAQ",
   tips: "Tips & Tricks",
   "ai-assistant": "How to use the AI Assistant?",
-  "not-supported-features": "Not supported features",
+  "not-supported-features": "Unsupported Features",
   updates: "Recent Updates",
 };
