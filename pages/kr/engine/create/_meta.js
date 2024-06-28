783d965ef0c0807f5377f84940f1e2ace4447287
theo <theo@redbrick.space>
2024-06-28 17:32:27 +0900

    1. fixed mistranslation 2. created page ssafy hall of fame 3. Add supplementary explanation

--- pages/kr/engine/create/_meta.js
+++ pages/kr/engine/create/_meta.js
@@ -1,4 +1,4 @@
 export default {
   access: "엔진에 액세스하는 방법",
-  operate: "엔진에서 작동하는 방법",
+  operate: "엔진 화면 조작 방법",
 };
--- pages/kr/ssafy12/_meta.js
+++ pages/kr/ssafy12/_meta.js
@@ -2,5 +2,5 @@ export default {
   "question-guide": "❓ 디스코드 질문 매뉴얼",
   "snippet-world": " 💃 스니펫 월드 사용방법",
   tips: "🍯 3D 콘텐츠 제작 꿀팁",
-  'hall-of-fame': "🏆 명예의 전당"
+  "hall-of-fame": "🏆 명예의 전당"
 };
