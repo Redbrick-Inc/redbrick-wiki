3b7d4925eec6a34638d5a2a817b953daf8960bb2
theo <theo@redbrick.space>
2024-07-02 12:26:59 +0900

    1. added ssafy learning materials page 2. wiki detail update

--- pages/kr/ssafy12/_meta.js
+++ pages/kr/ssafy12/_meta.js
@@ -2,5 +2,6 @@ export default {
   "question-guide": "❓ 디스코드 질문 매뉴얼",
   "snippet-world": " 💃 스니펫 월드 사용방법",
   tips: "🍯 3D 콘텐츠 제작 꿀팁",
-  "hall-of-fame": "🏆 명예의 전당"
+  "hall-of-fame": "🏆 명예의 전당",
+  "learning-materials": "📝 학습 자료",
 };
