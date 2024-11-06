07123818e824765cb2b1cb900a33215a88c210a9
Dave <dave@redbrick.space>
2024-11-06 17:39:09 +0900

    Add 3D OBJECT Category - Mod image path of 3D OBJECT stuffs

---
+++ pages/en/snippet/3d-object/clone/_meta.js
@@ -0,0 +1,4 @@
+export default {
+  "clone-object" : "Clone Object",
+  "clone-audio" : "Clone Audio",
+};
--- pages/kr/snippet/_meta.js
+++ pages/kr/snippet/_meta.js
@@ -2,17 +2,9 @@ export default {
   'redbrick-extension' : '레드브릭 확장',
   'game-flow' : '게임 흐름',
   event : '이벤트',
-  "management-multiple-similar-objects" : "다수의 유사한 오브젝트 관리",
+  '3d-object' : '3D 오브젝트',
   'gui-object' : 'GUI 오브젝트',
   tween : 'TWEEN',
   tips : '팁',
-  "continuously-tracking-player" : "플레이어를 지속적으로 추적하는 오브젝트 만들기",
-  "Making-object-face-to-another" : "오브젝트가 항상 특정 객체를 바라보게 만들기",
-  "random-position" : "랜덤한 위치 설정하기",
-  "change-avatar-appearance" : "아바타 모습 바꾸기",
-  "object-rotation" : "오브젝트 지속적으로 회전시키기",
-  "how-to-use-clone" : "오브젝트를 복제하여 사용하기",
-  "clone-audio" : "오디오를 복제하여 사용하기",
   'sample-game' : '샘플 게임',
-  'add-text-to-world' : 'WORLD에 텍스트 추가'
 };
