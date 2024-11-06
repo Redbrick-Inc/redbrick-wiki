d2a29d974ec6848d6effb26def05463ca41746dc
Dave <dave@redbrick.space>
2024-11-06 16:06:49 +0900

    Add TIPS Category - Mod image path of TIPS stuffs - Mod invalid discord link

---
+++ pages/en/snippet/tips/_meta.js
@@ -0,0 +1,6 @@
+export default {
+  debug : "Debug and Troubleshoot Errors",
+  "quarter-view-camera" : "Quarter View Camera",
+  "local-storage" : "Save data to Local Storage",
+  "change-background" : "Change the Background image during playing",
+};
