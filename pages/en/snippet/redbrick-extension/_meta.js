0dd9fbfcafdbfbcbebf0c53b2d85874514bc7789
Dave <dave@redbrick.space>
2024-11-06 11:12:07 +0900

    Add REDBRICK EXTENSION Category - Mod image path of REDBRICK EXTENSION Stuffs

---
+++ pages/en/snippet/redbrick-extension/_meta.js
@@ -0,0 +1,8 @@
+export default {
+  spawn: 'Starting Point',
+  'change-avatar-speed' : 'Change Avatar Speed',
+  respawn : 'Return to original position when it falls',
+  savepoint : 'Save Point',
+  timer : 'Timer, Countdown',
+  portal : 'Switch to another game',
+};
---
+++ pages/kr/snippet/redbrick-extension/_meta.js
@@ -0,0 +1,8 @@
+export default {
+  spawn : '시작점 설정',
+  'change-avatar-speed' : '아바타 속도 변경',
+  respawn : '떨어졌을 때 원래 위치로 돌아가기',
+  savepoint : '세이브 포인트',
+  timer : '타이머, 카운트다운',
+  portal : '다른게임으로 전환',
+};
