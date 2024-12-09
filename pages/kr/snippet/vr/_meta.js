4415c602c73d0412f8fc227f65f84d93aa9ee847
seonga-redbrick <seonga@redbirck.space>
2024-12-09 16:48:39 +0900

    Modify VR introduction (add test without VR device) + add VR controller function introduction (remove default VR settings, trigger, Grab)

--- pages/kr/snippet/vr/_meta.js
+++ pages/kr/snippet/vr/_meta.js
@@ -1,4 +1,5 @@
 export default {
     introduce: '소개',
+    controller : 'Controller 관련 함수 소개',
     example: '예제',
 };
---
+++ pages/kr/snippet/vr/controller/_meta.js
@@ -0,0 +1,5 @@
+export default {
+    'Remove-Basic-VR': '기본 VR 설정 제거',
+    'Trigger': 'Trigger 관련 함수 소개',
+    'Grab': 'Grab 관련 함수 소개'
+};
