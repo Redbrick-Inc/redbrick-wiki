0d37ef5192c164d04e031ac3fb862dd61486fcef
Dave-Redbrick <dave@redbrick.space>
2024-12-20 14:44:53 +0900

    fix: typos and naming

--- pages/kr/engine/_meta.js
+++ pages/kr/engine/_meta.js
@@ -2,7 +2,7 @@ export default {
   "content-type": "Content Type",
   create: "월드 생성하기",
   operate: "패널 및 툴바 조작하기",
-  "edit-camera-related": "편집 카메라 조작법",
+  "edit-camera-related": "편집 카메라 조작 기능",
   "grouping-and-save-asset": "그룹화 및 에셋으로 저장하기",
   "replace-asset": "에셋 교체하기",
 };
