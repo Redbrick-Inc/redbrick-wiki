9fce463160233a7e544873b8920e56c727b0b16b
Tim <139097769+tim-redbrick@users.noreply.github.com>
2024-12-20 17:00:55 +0900

    add: tailwind and fonts

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,5 @@
-import { GoogleTagManager } from '@next/third-parties/google';
+import { GoogleTagManager } from "@next/third-parties/google";
+import "../global.css";
 
 function MyApp({ Component, pageProps }) {
   return (
