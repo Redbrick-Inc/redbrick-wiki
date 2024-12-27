23dc93db6e51ce7fcf726fe21b83e8b060f61761
Dave-Redbrick <dave@redbrick.space>
2024-12-27 17:30:18 +0900

    Add apply-and-explore-further Page 1. localStorage and Portal part 2. Rank part 3. collideByDistance part 4. sample game part

--- pages/kr/ssafy13/_meta.js
+++ pages/kr/ssafy13/_meta.js
@@ -6,7 +6,7 @@ export default {
   "must-know": "‼️ 반드시 알아야 하는 내용",
   "question-guide": "❓ 디스코드 질문 메뉴얼",
   tips: "🍯 레드브릭 엔진 꿀팁",
-  "how-to-use-sample-game": "🎮 샘플 게임 활용방법",
+  "apply-and-explore-further": "📖 응용 및 더 알아보기",
   "hall-of-fame": "🏆 지난 기수 작품보기",
   "learning-materials": "📝 학습 자료",
   "snippet-world": {
